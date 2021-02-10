18ba6b8bde3a4e231e3d96044bfbcdfd9acd3c12
Jaeri Park <59538964+motonim@users.noreply.github.com>
2021-02-09 21:51:01 -0500

    add examples

--- src/index.js
+++ src/index.js
@@ -2,10 +2,57 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
+// function double(x) {
+//   return x * 2;
+// }
+
+// const newNumbers = numbers.map(double);
+// console.log(newNumbers);
+
+// var newNumbers = [];
+
+// function double(x) {
+//   newNumbers.push(x * 2);
+// }
+
+// numbers.forEach(double);
+// console.log(newNumbers);
+
 //Filter - Create a new array by keeping the items that return true.
+
+// const newNumbers = numbers.filter(function (num) {
+//   return num > 10;
+// });
+
+// console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
+// var newNumber = numbers.reduce(function(accumulator, currentNumber) {
+//   return accumulator + currentNumber
+// });
+
+// console.log(newNumber)
+
+// var newNumber = 0;
+// numbers.forEach(function (currentNumber) {
+//   return (newNumber += currentNumber);
+// });
+
+// console.log(newNumber);
+
 //Find - find the first item that matches from an array.
 
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// });
+
+// console.log(newNumber);
+
 //FindIndex - find the index of the first item that matches.
+
+// const newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// });
+
+// console.log(newNumber);
